bfcff6823467aff6025d09b63c49b3e612289786
root <root@localhost>
2022-01-30 04:38:52 +0000

    Add tests for new attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,10 +1,14 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
+require 'time'
 
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J K Rowling'
+    fill_in 'Price', with: 19.99
+    fill_in 'Publish Date', with: Date.new(2007, 7, 21)
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,9 +1,13 @@
 # location: spec/unit/unit_spec.rb
 require 'rails_helper'
+require 'time'
 
 RSpec.describe Book, type: :model do
   subject do
     described_class.new(title: 'harry potter')
+    described_class.new(author: 'J K Rowling')
+    described_class.new(price: 19.99)
+    described_class.new(date: Date.new(2007, 7, 21))
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +16,21 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a publish date' do
+    subject.date = nil
     expect(subject).not_to be_valid
   end
 end
