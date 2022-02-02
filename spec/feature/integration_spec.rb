3230a553ac92d9ad11a89a1cf7457feab68de56c
root <root@localhost>
2022-02-02 17:33:03 +0000

    Fix unit and integration tests to be properly formatted

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J K Rowling'
     fill_in 'Price', with: 19.99
-    fill_in 'Publish Date', with: Date.new(2007, 7, 21)
+    select('2020', from: 'book_date_1i')
+    select('January', from: 'book_date_2i')
+    select('31', from: 'book_date_3i')
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
     expect(page).to have_content('J K Rowling')
     expect(page).to have_content('19.99')
-    expect(page).to have_content('2007-7-21')
+    expect(page).to have_content('2020-01-31')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,13 +1,14 @@
 # location: spec/unit/unit_spec.rb
 require 'rails_helper'
-require 'time'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
-    described_class.new(author: 'J K Rowling')
-    described_class.new(price: 19.99)
-    described_class.new(date: Date.new(2007, 7, 21))
+    described_class.new(
+        title: 'harry potter',
+        author: 'J K Rowling',
+        price: 19.99,
+        date:'2020-01-31'
+        )
   end
 
   it 'is valid with valid attributes' do
@@ -17,20 +18,24 @@ RSpec.describe Book, type: :model do
   it 'is not valid without a name' do
     subject.title = nil
     expect(subject).not_to be_valid
+    subject.title = 'harry potter'
   end
 
   it 'is not valid without an author' do
     subject.author = nil
     expect(subject).not_to be_valid
+    subject.author = 'J K Rowling'
   end
 
   it 'is not valid without a price' do
     subject.price = nil
     expect(subject).not_to be_valid
+    subject.price = 19.99
   end
 
   it 'is not valid without a publish date' do
     subject.date = nil
     expect(subject).not_to be_valid
+    subject.date = '2020-01-31'
   end
 end
