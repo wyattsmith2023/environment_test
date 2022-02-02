46a34c7b512c2b8f59a0e6ccd1b51d454bbe1678
wyattsmith2023 <57771792+wyattsmith2023@users.noreply.github.com>
2022-02-02 11:42:43 -0600

    Add new attributes to Book class

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_170538) do
+ActiveRecord::Schema.define(version: 2022_02_02_164736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.string "author"
+    t.decimal "price"
+    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
