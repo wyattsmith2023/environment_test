64b3d8d8091209afd541c03318bf53830845bcbd
root <root@localhost>
2022-02-10 21:22:17 +0000

    Commit for Lab 3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+    has_many :user
     validates :title, presence: true
     validates :author, presence: true
     validates :price, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
+  resources :books
+
+  root 'user_books#index'
+
   resources :books do 
     member do
       get :delete
     end
   end
-  root 'books#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
